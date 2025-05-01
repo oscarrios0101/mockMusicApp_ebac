e04c3a5cc3de58c8fc787c2d422f21655955a02d
Oscar Rios <oscarrios0101@gmail.com>
2025-04-30 22:47:39 -0600

    github pages bugfix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_ebac_practica_3/",
+  base: "/mockMusicApp_ebac",
   build: {
     sourcemap: true,
   },
